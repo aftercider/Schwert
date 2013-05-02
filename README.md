cc8da9e3d7268058e8be9ef130970f2465b1df06
Aftercider <aftercider@gmail.com>
2013-05-02 12:26:26 +0900

    Android2.1向けパッケージに変更 必要なActivityを空Activityで用意

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 com.aftercider.schwert.*
 
 - Activity
-  - HomeActivity
-  - ImageActivity
-  - SettingActivity
+  - HomeActivity:ホームメニューとなるActivity
+  - DrawingActivity:みんなが絵を描くActivity
+  - ImportingActivity:画像を読み込むよう説明するActivity
+  - SettingActivity:読み込んだ画像をズーム・移動するActivity
+  - ConfigActivity:各種アプリの設定を行うActivity
---
+++ src/com/aftercider/schwert/ImportingActivity.java
@@ -0,0 +1,20 @@
+package com.aftercider.schwert;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ImportingActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_importing);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.brower, menu);
+		return true;
+	}
+}
