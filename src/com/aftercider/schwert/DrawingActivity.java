70511157c411765178da8d084aa996410f5a04df
Aftercider <aftercider@gmail.com>
2013-05-02 13:36:33 +0900

    各種ボタン配置

--- src/com/aftercider/schwert/DrawingActivity.java
+++ src/com/aftercider/schwert/DrawingActivity.java
@@ -1,14 +1,59 @@
 package com.aftercider.schwert;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.app.Activity;
-import android.view.Menu;
 
 public class DrawingActivity extends Activity {
 
+	private boolean mIsShownSettings = false;
+	private Button mButtonToImporting = null;
+	private ImageButton mImageButtonToSetting = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_drawing);
+		
+		createWidgets();
+	}
+	
+	private void createWidgets(){
+		mButtonToImporting = null; // TODO: 作る
+		mImageButtonToSetting = (ImageButton)findViewById(R.id.imageButtonDrawingToSetting);
+		
+		/*mButtonToImporting.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		*/
+		
+		mImageButtonToSetting.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				showSettings(true);
+			}
+		});
+	}
+	
+	/**
+	 * 画面下部に設定用パネルを表示して、画像をズーム・移動可能にする。
+	 * 
+	 * @param isShow trueなら表示して、falseなら隠す
+	 */
+	public void showSettings(boolean isShow){
+		if(isShow == mIsShownSettings){
+			return;
+		}else{
+			mIsShownSettings = isShow;
+		}
 	}
 }
--- src/com/aftercider/schwert/HomeActivity.java
+++ src/com/aftercider/schwert/HomeActivity.java
@@ -2,13 +2,50 @@ package com.aftercider.schwert;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
+/**
+ * 起動時に表示されるActivity。
+ * ロゴやバージョン、メイン画面へのボタン、コンフィグへのボタンが表示される。
+ * 
+ * @author Aftercider
+ *
+ */
 public class HomeActivity extends Activity {
-
+	
+	private Button mButtonToDrawing = null;
+	private Button mButtonToConfig  = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		
+		createWidgets();
+	}
+	
+	private void createWidgets() {
+		mButtonToDrawing = (Button)findViewById(R.id.buttonHomeToDrawing);
+		mButtonToConfig  = (Button)findViewById(R.id.buttonHomeToConfig);
+		
+		mButtonToDrawing.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setClassName("com.aftercider.schwert","com.aftercider.schwert.DrawingActivity");
+				startActivity(intent);
+			}
+		});
+		mButtonToConfig.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setClassName("com.aftercider.schwert","com.aftercider.schwert.ConfigActivity");
+				startActivity(intent);
+			}
+		});
 	}
 }
